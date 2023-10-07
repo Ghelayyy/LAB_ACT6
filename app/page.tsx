1ff6e6f20591750271c58e84494f66423b724b0a
Ghelayyy <occ.bermudez.ghelmarie@gmail.com>
2023-10-07 14:21:34 +0800

    sample commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React, { useState } from 'react';
+export default function Home() {
+  const [number, setNum] = useState(0);
+
+  const increment = () => { 
+    setNum((prevNum) => prevNum + 1); 
+  };
+
+  const decrement = () => {
+    setNum((prevNum) => prevNum - 1); 
+  }; 
+  
+  return (
+    <div>
+    <span><h1>COUNTER</h1></span>
+    <span>counter: {number}</span>
+
+        <div className='bContainer'>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </div> 
+    </div>
+  );
+}
